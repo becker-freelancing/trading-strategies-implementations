0f0d007b07e0d7c2f788e70fae90738e0f236693
jasb <jason.becker@parcIT.de>
2025-01-21 14:19:23 +0100

    BacktestApp

---
+++ trading-app/src/main/java/com/becker/freelance/app/backtest/BacktestApp.java
@@ -0,0 +1,12 @@
+package com.becker.freelance.app.backtest;
+
+import com.becker.freelance.app.AbstractBacktestApp;
+
+import java.io.IOException;
+
+public class BacktestApp {
+
+    public static void main(String[] args) throws IOException {
+        AbstractBacktestApp.main(args);
+    }
+}
