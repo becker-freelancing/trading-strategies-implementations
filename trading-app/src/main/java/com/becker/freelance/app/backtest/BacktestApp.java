8de224797de8221ba4e39109cc6ec6f409eed635
jasb <jason.becker@parcIT.de>
2025-01-24 07:50:19 +0100

    Backtest App refactored

--- trading-app/src/main/java/com/becker/freelance/app/backtest/BacktestApp.java
+++ trading-app/src/main/java/com/becker/freelance/app/backtest/BacktestApp.java
@@ -3,10 +3,17 @@ package com.becker.freelance.app.backtest;
 import com.becker.freelance.app.AbstractBacktestApp;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class BacktestApp {
 
     public static void main(String[] args) throws IOException {
-        AbstractBacktestApp.main(args);
+        AbstractBacktestApp backtestApp = AbstractBacktestApp.builder()
+                .withInitialWalletAmount(2000.)
+                .withFromTime(LocalDateTime.parse("2023-06-01T00:00:00"))
+                .withToTime(LocalDateTime.parse("2024-01-01T00:00:00"))
+                .build();
+
+        backtestApp.run();
     }
 }
