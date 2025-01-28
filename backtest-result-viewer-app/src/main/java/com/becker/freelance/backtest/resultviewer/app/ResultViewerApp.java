418659622d6459ac4d561899da61d154356debb2
jasb <jason.becker@parcIT.de>
2025-01-28 07:02:23 +0100

    Backtest Result Viewer

---
+++ backtest-result-viewer-app/src/main/java/com/becker/freelance/backtest/resultviewer/app/ResultViewerApp.java
@@ -0,0 +1,8 @@
+package com.becker.freelance.backtest.resultviewer.app;
+
+public class ResultViewerApp {
+
+    public static void main(String[] args) {
+        new AbstractBacktestResultViewerApp().run();
+    }
+}
