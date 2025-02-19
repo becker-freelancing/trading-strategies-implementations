c472a691cbc4f7e2d1530abec60d407ac7f59d5b
jasb <jason.becker@parcIT.de>
2025-02-19 11:46:38 +0100

    Time Frame Converter

--- zpython/preanalysis/time_frame/time_frame_converter.py
+++ zpython/preanalysis/time_frame/time_frame_converter.py
@@ -1,21 +1,27 @@
 from zpython.util.hist_data_data_reader import read_data
 
-write_path = "C:/Users/jasb/AppData/Roaming/krypto-java/data-histdata/EURUSD_5.csv.zip"
 input = read_data("EURUSD_1.csv.zip")
 
-in_re = input.resample('5min').agg({
-    "openBid": "first",
-    "openAsk": "first",
-    "highBid": "max",
-    "highAsk": "max",
-    "lowBid": "min",
-    "lowAsk": "min",
-    "closeBid": "last",
-    "closeAsk": "last"
-})
-valid_counts = input.resample('5min').count()
-in_re = in_re.where(valid_counts >= 3)
-in_re = in_re.dropna()
-in_re.reset_index(inplace=True)
 
-in_re.to_csv(write_path, compression="zip", index=False)
+def for_tf(tf_min):
+    write_path = f"C:/Users/jasb/AppData/Roaming/krypto-java/data-histdata/EURUSD_{tf_min}.csv.zip"
+    in_re = input.resample(f'{tf_min}min').agg({
+        "openBid": "first",
+        "openAsk": "first",
+        "highBid": "max",
+        "highAsk": "max",
+        "lowBid": "min",
+        "lowAsk": "min",
+        "closeBid": "last",
+        "closeAsk": "last"
+    })
+    valid_counts = input.resample(f'{tf_min}min').count()
+    # in_re = in_re.where(valid_counts >= 10)
+    in_re = in_re.dropna()
+    in_re.reset_index(inplace=True)
+
+    in_re.to_csv(write_path, compression="zip", index=False)
+
+
+for tf_min in [5, 15, 30, 60, 1440]:
+    for_tf(tf_min)
