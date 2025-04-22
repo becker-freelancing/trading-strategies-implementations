e4a3d2de61dcbc793ba49a381c096b600ca7e826
jasb <jason.becker@parcIT.de>
2025-04-22 07:02:33 +0200

    LSTM Models

---
+++ zpython/training/regression/encode_decode_lstm_regression_trainer.py
@@ -0,0 +1,69 @@
+from keras import Model
+from keras.api.layers import Dense, InputLayer, LSTM, RepeatVector, TimeDistributed
+from keras.api.models import Sequential
+from keras.api.optimizers import Adam
+from optuna import Trial
+from sklearn.preprocessing import MinMaxScaler
+
+from zpython.training.regression.regression_model_trainer import RegressionModelTrainer
+
+
+class NNRegressionTrainer(RegressionModelTrainer):
+
+    def __init__(self):
+        super().__init__("encodedecodelstm", MinMaxScaler)
+
+    def _get_output_length(self):
+        return 30
+
+    def _get_target_column(self):
+        return "logReturn_closeBid_1min"
+
+    def _get_max_input_length(self) -> int:
+        return 150
+
+    def _create_model(self, trial: Trial) -> (Model, int, dict):
+        # Hyperparameter von Optuna
+        num_units_input = trial.suggest_int('num_units_input', 32, 128)  # Anzahl der Neuronen pro Schicht
+        num_units = trial.suggest_int('num_units', 32, 128)  # Anzahl der Neuronen pro Schicht
+        learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-2, log=True)  # Lernrate
+        input_length = trial.suggest_int('input_length', 30, 150)
+
+        params = {
+            "num_units": num_units,
+            "learning_rate": learning_rate,
+            "input_length": input_length,
+            "num_units_input": num_units_input
+        }
+
+        # Modell erstellen
+        model = Sequential()
+        model.add(InputLayer(shape=(input_length, 52)))
+        model.add(LSTM(num_units_input, return_sequences=False))
+
+        model.add(RepeatVector(30))
+        model.add(LSTM(num_units, activation="relu", return_sequences=True))
+        model.add(TimeDistributed(Dense(self._get_output_length())))
+
+        # Kompilieren des Modells
+        model.compile(optimizer=Adam(learning_rate=learning_rate), loss='mean_squared_error',
+                      metrics=self._get_metrics())
+
+        return model, input_length, params
+
+    def _get_optuna_trial_params(self) -> list[str]:
+        return ["num_layers", "num_units", "learning_rate"]
+
+    def _get_max_epochs_to_train(self):
+        return 30
+
+    def _get_optuna_optimization_metric_name(self):
+        return 'val_rmse'
+
+    def _get_optuna_optimization_metric_direction(self):
+        return 'minimize'
+
+
+if __name__ == "__main__":
+    trainer = NNRegressionTrainer()
+    trainer.train_model()
