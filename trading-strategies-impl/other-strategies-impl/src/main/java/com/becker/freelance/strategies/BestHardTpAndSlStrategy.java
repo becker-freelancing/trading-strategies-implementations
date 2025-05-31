2ef1ef93586b2cc49d9acd606820938f7fc3d0b4
jasb <jason.becker@parcIT.de>
2025-05-31 08:35:37 +0200

    Strategy refactoring

--- trading-strategies-impl/other-strategies-impl/src/main/java/com/becker/freelance/strategies/BestHardTpAndSlStrategy.java
+++ trading-strategies-impl/other-strategies-impl/src/main/java/com/becker/freelance/strategies/BestHardTpAndSlStrategy.java
@@ -3,55 +3,37 @@ package com.becker.freelance.strategies;
 import com.becker.freelance.commons.position.Direction;
 import com.becker.freelance.commons.position.PositionType;
 import com.becker.freelance.commons.signal.EntrySignal;
-import com.becker.freelance.commons.signal.EntrySignalFactory;
 import com.becker.freelance.commons.signal.ExitSignal;
 import com.becker.freelance.math.Decimal;
-import com.becker.freelance.strategies.init.PermutableStrategyInitParameter;
-import com.becker.freelance.strategies.init.StrategyInitParameter;
-import com.becker.freelance.strategies.parameter.EntryParameter;
-import com.becker.freelance.strategies.parameter.ExitParameter;
+import com.becker.freelance.strategies.creation.StrategyCreator;
+import com.becker.freelance.strategies.executionparameter.EntryParameter;
+import com.becker.freelance.strategies.executionparameter.ExitParameter;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 public class BestHardTpAndSlStrategy extends BaseStrategy{
 
-    public BestHardTpAndSlStrategy(){
-        super("Best_Hard_TP_and_SL", new PermutableStrategyInitParameter(List.of(
-                new StrategyInitParameter("tp", 30, 50, 1000, 50),
-                new StrategyInitParameter("sl", 15, 50, 1000, 50),
-                new StrategyInitParameter("all_buy", 0, 0, 1, 1)
-        )));
-    }
+    private final boolean allBuy;
+    private final Decimal tp;
+    private final Decimal sl;
 
-    private boolean allBuy;
-    private Decimal tp;
-    private Decimal sl;
-    private EntrySignalFactory entrySignalFactory;
-
-    public BestHardTpAndSlStrategy(Map<String, Decimal> parameters) {
-        super(parameters);
-        allBuy = !parameters.get("all_buy").isEqualToZero();
-        tp = parameters.get("tp");
-        sl = parameters.get("sl");
-        entrySignalFactory = new EntrySignalFactory();
+    public BestHardTpAndSlStrategy(StrategyCreator strategyCreator, boolean allBuy, Decimal tp, Decimal sl) {
+        super(strategyCreator);
+        this.allBuy = allBuy;
+        this.tp = tp;
+        this.sl = sl;
     }
 
     @Override
-    public Optional<EntrySignal> shouldEnter(EntryParameter entryParameter) {
+    public Optional<EntrySignal> internalShouldEnter(EntryParameter entryParameter) {
 
 
         return Optional.of(entrySignalFactory.fromDistance(Decimal.ONE, allBuy ? Direction.BUY : Direction.SELL, sl, tp, PositionType.HARD_LIMIT, entryParameter.currentPrice()));
     }
 
     @Override
-    public Optional<ExitSignal> shouldExit(ExitParameter exitParameter) {
+    public Optional<ExitSignal> internalShouldExit(ExitParameter exitParameter) {
         return Optional.empty();
     }
 
-    @Override
-    public BaseStrategy forParameters(Map<String, Decimal> parameters) {
-        return new BestHardTpAndSlStrategy(parameters);
-    }
 }
---
+++ trading-strategies-impl/other-strategies-impl/src/main/java/com/becker/freelance/strategies/BestHardTpAndSlStrategyCreator.java
@@ -0,0 +1,36 @@
+package com.becker.freelance.strategies;
+
+import com.becker.freelance.strategies.creation.*;
+import com.becker.freelance.strategies.validinitparameter.StrategyInitParameter;
+import com.becker.freelance.strategies.validinitparameter.ValidStrategyInitParameters;
+
+import java.util.List;
+
+public class BestHardTpAndSlStrategyCreator implements StrategyCreator {
+
+    private static final ParameterName ALL_BUY = new StringParameterName("allBuy");
+
+    @Override
+    public String strategyName() {
+        return "Best_Hard_TP_and_SL";
+    }
+
+    @Override
+    public ValidStrategyInitParameters strategyParameters() {
+        return new ValidStrategyInitParameters(List.of(
+                new StrategyInitParameter(DefaultParameterNames.TAKE_PROFIT, 30, 50, 1000, 50),
+                new StrategyInitParameter(DefaultParameterNames.STOP_LOSS, 15, 50, 1000, 50),
+                new StrategyInitParameter(ALL_BUY, 0, 0, 1, 1)
+        ));
+    }
+
+    @Override
+    public TradingStrategy build(StrategyParameter parameter) {
+        return new BestHardTpAndSlStrategy(
+                this,
+                parameter.getParameterAsBool(ALL_BUY),
+                parameter.getParameter(DefaultParameterNames.TAKE_PROFIT),
+                parameter.getParameter(DefaultParameterNames.STOP_LOSS)
+        );
+    }
+}
