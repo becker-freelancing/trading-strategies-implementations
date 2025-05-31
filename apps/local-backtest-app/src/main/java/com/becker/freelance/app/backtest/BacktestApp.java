4d3449f33ef2de2cefa541a4fdb43f473ff09887
jasb <jason.becker@parcIT.de>
2025-05-31 10:26:59 +0200

    Regime Indicators

--- apps/local-backtest-app/src/main/java/com/becker/freelance/app/backtest/BacktestApp.java
+++ apps/local-backtest-app/src/main/java/com/becker/freelance/app/backtest/BacktestApp.java
@@ -10,8 +10,8 @@ public class BacktestApp {
     public static void main(String[] args) {
         Runnable backtestApp = AbstractLocalBacktestAppBuilder.builder()
                 .withInitialWalletAmount(new Decimal(200_000))
-                .withFromTime(LocalDateTime.parse("2025-03-30T00:00:00"))
-                .withToTime(LocalDateTime.parse("2025-04-06T00:00:00"))
+                .withFromTime(LocalDateTime.parse("2024-05-01T00:00:00"))
+                .withToTime(LocalDateTime.parse("2025-04-01T00:00:00"))
                 .build();
 
         backtestApp.run();
