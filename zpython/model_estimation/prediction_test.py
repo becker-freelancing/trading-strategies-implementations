51e1789c4afddc14801d2ee3af93d3cd445dfbb6
jasb <jason.becker@parcIT.de>
2025-06-14 07:18:05 +0200

    Python CleanUp

--- zpython/model_estimation/prediction_test.py
+++
@@ -1,7 +0,0 @@
-from zpython.model.best_model_loader import load_best_models
-from zpython.model.regime_model import LoadedModelProvider, RegimeLiveModel
-
-models = load_best_models("modely-bybit/SEQUENCE_PREDICTION")
-
-provider = LoadedModelProvider(models)
-model = RegimeLiveModel(provider)  # TODO
