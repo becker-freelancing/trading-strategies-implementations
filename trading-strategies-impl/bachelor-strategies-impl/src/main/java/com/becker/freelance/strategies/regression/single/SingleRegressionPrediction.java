70987a8001b3c85fa8903131d65db785328a0ca9
jasb <jason.becker@parcIT.de>
2025-08-19 10:48:23 +0200

    Regimes can be divided in durations or not

--- trading-strategies-impl/bachelor-strategies-impl/src/main/java/com/becker/freelance/strategies/regression/single/SingleRegressionPrediction.java
+++ trading-strategies-impl/bachelor-strategies-impl/src/main/java/com/becker/freelance/strategies/regression/single/SingleRegressionPrediction.java
@@ -1,12 +1,12 @@
 package com.becker.freelance.strategies.regression.single;
 
-import com.becker.freelance.commons.regime.TradeableQuantilMarketRegime;
+import com.becker.freelance.commons.regime.TradeableMarketRegime;
 import com.becker.freelance.strategies.shared.LogReturnInverseTransformer;
 
 import java.time.LocalDateTime;
 
 public record SingleRegressionPrediction(LocalDateTime closeTime,
-                                         TradeableQuantilMarketRegime regime,
+                                         TradeableMarketRegime regime,
                                          Double min,
                                          Double max,
                                          LogReturnInverseTransformer<Double> minInverseTransformer,
